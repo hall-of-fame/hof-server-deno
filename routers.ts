f5bec9ac9834207545502f5e30c67dfc902a32fa
lumingrui <lomirus@qq.com>
2021-09-15 17:45:57 +0800

    feat: new api `GET /multiple`

--- routers.ts
+++ routers.ts
@@ -1,12 +1,13 @@
 import { Context } from "https://deno.land/x/oak@v9.0.0/mod.ts";
 import departmentsData from "./services/departments.ts";
+import multipleData from "./services/multiple.ts";
 
 function departments(ctx: Context) {
     ctx.response.body = departmentsData;
 }
 
 function multiple(ctx: Context) {
-    console.log("multiple")
+    ctx.response.body = multipleData;
 }
 
 function popular(ctx: Context) {
---
+++ services/multiple.ts
@@ -0,0 +1,21 @@
+const multiple: Array<{
+    author: string,
+    desc: string,
+    url: string
+}> = [];
+const entryPath = "./static/multiple";
+
+for await (const sticker of Deno.readDir(entryPath)) {
+    const res = sticker.name.match(/(.*?)\-(.*)\..*/);
+    if (res === null) {
+        console.error(`Illegal Filename: ${sticker.name}`);
+        Deno.exit();
+    }
+    multiple.push({
+        author: res[1],
+        desc: res[2],
+        url: `/static/multiple/${sticker.name}`
+    })
+}
+
+export default multiple;
