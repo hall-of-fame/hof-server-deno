00a0e6d623618a8483510df030934aec72dc7ef8
lumingrui <lomirus@qq.com>
2021-09-16 19:52:32 +0800

    refactor(middleware): encapsule static request handler

--- middlewares/root.ts
+++ middlewares/root.ts
@@ -3,20 +3,24 @@ import { Context } from "https://deno.land/x/oak@v9.0.0/mod.ts";
 
 async function root(ctx: Context, next: () => Promise<unknown>) {
     if (ctx.request.url.pathname.startsWith("/static/")) {
-        try {
-            await send(ctx, ctx.request.url.pathname.slice(8), {
-                root: `${Deno.cwd()}/static/`,
-            })
-        } catch (error) {
-            if (error.name === "NotFoundError") {
-                const pathname = decodeURI(ctx.request.url.pathname).replaceAll(" ", "%20");
-                console.log(`404: ${pathname}`)
-            }
-        }
+        await handleStaticRequest(ctx);
     } else {
         await next();
         ctx.response.body = {
             data: ctx.response.body
+        }
+    }
+}
+
+async function handleStaticRequest(ctx: Context) {
+    try {
+        await send(ctx, ctx.request.url.pathname.slice(8), {
+            root: `${Deno.cwd()}/static/`,
+        })
+    } catch (error) {
+        if (error.name === "NotFoundError") {
+            const pathname = decodeURI(ctx.request.url.pathname).replaceAll(" ", "%20");
+            console.log(`404: ${pathname}`)
         }
     }
 }
