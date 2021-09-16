c49887962f863366a8e25be11266a8c2252160b4
lumingrui <lomirus@qq.com>
2021-09-16 19:36:22 +0800

    refactor(middleware): encapsule the root middleware

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
-import { Application, Router, send } from "https://deno.land/x/oak@v9.0.0/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak@v9.0.0/mod.ts";
 import { departments, multiple, popular } from "./routers.ts";
 import { hostname, port, password } from "./config/config.ts"
 import auth from "./middlewares/auth.ts";
+import root from "./middlewares/root.ts";
 
 const app = new Application();
 const router = new Router()
@@ -14,32 +15,12 @@ if (password) {
     app.use(auth);
 }
 
-app.use(async (ctx, next) => {
-    if (ctx.request.url.pathname.startsWith("/static/")) {
-        try {
-            await send(ctx, ctx.request.url.pathname.slice(8), {
-                root: `${Deno.cwd()}/static/`,
-            })
-        } catch (error) {
-            if (error.name === "NotFoundError") {
-                const pathname = decodeURI(ctx.request.url.pathname).replaceAll(" ", "%20");
-                console.log(`404: ${pathname}`)
-            }
-        }
-    } else {
-        await next();
-        ctx.response.body = {
-            data: ctx.response.body
-        }
-    }
-})
+app.use(root);
 
 app.use(router.routes());
 
 app.addEventListener("listen", ({ hostname, port }) => {
-    console.log(
-        `Listening on: http://${hostname}:${port}`,
-    );
+    console.log(`Listening on: http://${hostname}:${port}`);
 });
 
 await app.listen({ hostname, port });
---
+++ middlewares/root.ts
@@ -0,0 +1,24 @@
+import { send } from "https://deno.land/x/oak@v9.0.0/mod.ts";
+import { Context } from "https://deno.land/x/oak@v9.0.0/mod.ts";
+
+async function root(ctx: Context, next: () => Promise<unknown>) {
+    if (ctx.request.url.pathname.startsWith("/static/")) {
+        try {
+            await send(ctx, ctx.request.url.pathname.slice(8), {
+                root: `${Deno.cwd()}/static/`,
+            })
+        } catch (error) {
+            if (error.name === "NotFoundError") {
+                const pathname = decodeURI(ctx.request.url.pathname).replaceAll(" ", "%20");
+                console.log(`404: ${pathname}`)
+            }
+        }
+    } else {
+        await next();
+        ctx.response.body = {
+            data: ctx.response.body
+        }
+    }
+}
+
+export default root;
