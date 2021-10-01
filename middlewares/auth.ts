a4526d353ab36965b96b1f8cbd8c67643bba0840
lumingrui <lomirus@qq.com>
2021-10-01 16:50:47 +0800

    refactor: abandone the hash authorization

--- middlewares/auth.ts
+++ middlewares/auth.ts
@@ -1,4 +1,3 @@
-import { createHash } from "https://deno.land/std@0.107.0/hash/mod.ts";
 import { Context } from "https://deno.land/x/oak@v9.0.0/mod.ts";
 import { password } from "../config.ts"
 
@@ -6,10 +5,8 @@ if (password) {
     console.log("Authorization Enabled")
 }
 
-const hashStr = createHash("sha3-512").update(password ?? "").toString();
-
 async function auth(ctx: Context, next: () => Promise<unknown>) {
-    if (ctx.request.headers.get("Authorization") === hashStr) {
+    if (ctx.request.headers.get("Authorization") === password) {
         await next();
     } else {
         ctx.response.status = 401;
