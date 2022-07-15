b3b4ca52a83bc31533aec5ccbd362ba612c08885
lomirus <lomirus@qq.com>
2022-07-15 17:46:49 +0800

    refactor(config): replace `private.json` & `public.json` with one `config.json`

--- config.ts
+++ config.ts
@@ -1,22 +1,54 @@
 import { exists } from "https://deno.land/std@0.107.0/fs/mod.ts";
 
-// deno-lint-ignore no-explicit-any
-const config: Record<string, any> = {};
+type Config = {
+    hostname: string,
+    port: number,
+    /**
+     * If this option is set to `true`, the server
+     * will provide a secure connection, and try to
+     * find the certificate and private key in
+     * `./config/tls/full_chain.pem` and
+     * `./config/tls/private.key`.
+     */
+    secure: boolean,
+    /**
+     * If this option is specified, the request needs
+     * the `Authorization` in the headers. And if the
+     * value doesn't match the `password`, the server
+     * will respond with the status code 401.
+     */
+    password?: string,
+    /**
+     * This item's value will be responded when
+     * `GET /popular` is requested.
+     */
+    popular: Array<{
+        author: string,
+        desc: string,
+        url: string
+    }>,
+    /**
+     * It's a map of the username to its
+     * corresponding QQ id. So the client will then
+     * get the avatar url by the QQ id.
+     */
+    avatar: Record<string, string>
+};
 
-if (await exists("./config/public.json")) {
-    const publicData = await Deno.readTextFile("./config/public.json");
-    const publicConfig = JSON.parse(publicData);
-    Object.assign(config, publicConfig)
+async function getConfig(): Promise<Partial<Config>> {
+    if (await exists("./config.json")) {
+        const configText = await Deno.readTextFile("./config.json");
+        const config = JSON.parse(configText);
+        return config
+    } else {
+        return {}
+    }
 }
 
-if (await exists("./config/private.json")) {
-    const privateData = await Deno.readTextFile("./config/private.json");
-    const privateConfig = JSON.parse(privateData);
-    Object.assign(config, privateConfig)
-}
+const config = await getConfig();
 
-export const port: number = config.port;
-export const hostname: string = config.hostname;
+export const port: number = config.port ?? 8000;
+export const hostname: string = config.hostname ?? "127.0.0.1";
 export const popular: Array<{
     author: string,
     desc: string,
@@ -24,5 +56,4 @@ export const popular: Array<{
 }> = config.popular ?? [];
 export const avatar: Record<string, string> = config.avatar ?? {};
 export const password: string | undefined = config.password;
-export const secure: boolean = config.secure;
-
+export const secure: boolean = config.secure ?? false;
