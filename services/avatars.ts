be4f93cc5f297e3e22f8df390a472476e4150f63
lomirus <lomirus@qq.com>
2022-07-16 16:10:23 +0800

    feat: implement avatars api

--- services/avatars.ts
+++ services/avatars.ts
@@ -1,1 +1,12 @@
-export default {}
+import { avatars as srcAvatars } from '../config.ts'
+
+const avatars: Record<string, string> = {}
+Object.entries(srcAvatars).map(([key, value]) => {
+    if (typeof value === 'string') {
+        avatars[key] = value;
+    } else {
+        avatars[key] = `https://q1.qlogo.cn/g?b=qq&nk=${value}&s=640`;
+    }
+})
+
+export default avatars
